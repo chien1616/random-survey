ca744256eb7b00c72c53326238e411ae3a50b9ad
chien1616 <qian111690@gmail.com>
2024-12-10 17:16:24 +0800

    1210Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-
 	'https://docs.google.com/forms/d/e/1FAIpQLSe3AYn2AqlQ_4d8zl_KsYzTrFaciF6nnBSx034QpROkgHpdYg/viewform?usp=header',
 	'https://docs.google.com/forms/d/e/1FAIpQLScrYmN_ebG5LNhizqz19759IMXYTqKQ3Fg22PRjyKlHN2uncg/viewform?usp=header',
 ];
