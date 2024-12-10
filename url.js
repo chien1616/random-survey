ecf8ff74721adcb7c94b6ae80490824d5828016c
chien1616 <qian111690@gmail.com>
2024-12-10 18:05:39 +0800

    CAKEUpdate url.js

--- url.js
+++ url.js
@@ -8,6 +8,8 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLSe3AYn2AqlQ_4d8zl_KsYzTrFaciF6nnBSx034QpROkgHpdYg/viewform?usp=header',
-	'https://docs.google.com/forms/d/e/1FAIpQLScrYmN_ebG5LNhizqz19759IMXYTqKQ3Fg22PRjyKlHN2uncg/viewform?usp=header',
+	'https://www.surveycake.com/s/vNqP8',
+	'https://www.surveycake.com/s/xK24p',
+        'https://www.surveycake.com/s/86Pa1',
+	'https://www.surveycake.com/s/96Ln1',
 ];
