d7b9943b6e990cc8f62de74d06ff229442ebce36
chien1616 <qian111690@gmail.com>
2024-12-10 17:14:03 +0800

    1210Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
+
 	'https://docs.google.com/forms/d/e/1FAIpQLSe3AYn2AqlQ_4d8zl_KsYzTrFaciF6nnBSx034QpROkgHpdYg/viewform?usp=header',
 	'https://docs.google.com/forms/d/e/1FAIpQLScrYmN_ebG5LNhizqz19759IMXYTqKQ3Fg22PRjyKlHN2uncg/viewform?usp=header',
 ];
